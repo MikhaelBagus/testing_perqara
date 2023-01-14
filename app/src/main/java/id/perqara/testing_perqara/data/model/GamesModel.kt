59dd1a30c3fe9c86c3ffed15f544580a7b832597
Mikhael Bagus <bagus.mikhael@gmail.com>
2023-01-14 09:41:08 +0700

    ps

--- app/src/main/java/id/perqara/testing_perqara/data/model/GamesModel.kt
+++ app/src/main/java/id/perqara/testing_perqara/data/model/GamesModel.kt
@@ -8,7 +8,7 @@ data class GamesModel(
     @SerializedName("description") var description : String? = "",
     @SerializedName("released") var released : String? = "",
     @SerializedName("background_image") var background_image : String? = "",
-    @SerializedName("rating") var rating : Int? = 0,
+    @SerializedName("rating") var rating : Double? = 0.0,
     @SerializedName("publishers") var publishers : List<PublishersGamesModel>? = null,
     @SerializedName("playtime") var playtime : Int? = 0,
 )
